7ebbe4767d5d9e4e200e2e257a244b663e69520c
Repeater <532787738@qq.com>
2025-07-12 16:52:05 +0800

    用户上线鸡广播功能

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -64,6 +65,29 @@ func (server *Server) Handler(conn net.Conn) {
 
 	// 广播当前用户上线消息
 	server.Broadcast(user, "已上线")
+
+	// 接收客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				server.Broadcast(user, "下线")
+				return
+			}
+
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn Read err: ", err)
+				return
+			}
+
+			// 提取用户的消息(去除'\n')
+			msg := string(buf[:n-1])
+
+			// 将得到的消息广播
+			server.Broadcast(user, msg)
+		}
+	}()
 
 	// 当前 handler 阻塞
 	select {}
